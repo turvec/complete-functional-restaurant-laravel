6aef6cac9e59048a7bdfb95788362067c5be3595
turvec <okaforturvec@gmail.com>
2022-01-04 09:49:10 +0100

    changes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function allcustomers(Type $var = null)
     {
-        $customers = Auth::user()->all();
+        $customers = User::all();
         return view('admin.allcustomers', compact('customers'));
         # code...
     }
