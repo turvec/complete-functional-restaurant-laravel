a6f1c9f5362f1495b700ef53e8f096024d8ff7c4
turvec <okaforturvec@gmail.com>
2022-01-08 15:27:50 +0100

    changes

--- resources/views/admin/allchefs.blade.php
+++ resources/views/admin/allchefs.blade.php
@@ -62,8 +62,8 @@
                                                     <div class="dropdown"><button class="btn btn-primary tp-btn-light sharp" type="button" data-toggle="dropdown"><span class="fs--1"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg></span></button>
                                                         <div class="dropdown-menu dropdown-menu-right border py-0">
                                                             <div class="py-2">
-                                                                    {{-- <a class="dropdown-item" href="{{route('edit_chef',$chef->id)}}">Edit</a> --}}
-                                                                {{-- <a class="dropdown-item text-danger" href="{{route('delete_chef',$chef->id)}}">Delete</a> --}}
+                                                                <a class="dropdown-item" href="{{route('edit_chef',$chef->id)}}">Edit</a>
+                                                                <a class="dropdown-item text-danger" href="{{route('delete_chef',$chef->id)}}">Delete</a>
                                                             </div>
                                                         </div>
                                                     </div>
