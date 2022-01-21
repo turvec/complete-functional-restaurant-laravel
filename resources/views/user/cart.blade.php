6d6633e37dc4be5b5221c15ffcd92f16e93cd068
turvec <okaforturvec@gmail.com>
2022-01-21 01:35:28 +0100

    changes

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -77,7 +77,6 @@
                                 </tr>
 
                             </form>
-                            
                             @endforeach
 
                         </tbody>
