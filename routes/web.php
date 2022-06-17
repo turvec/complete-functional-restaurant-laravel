3bd6da4c7b20413699ec2489632cdae8a06e61b1
turvec <okaforturvec@gmail.com>
2022-06-17 13:04:30 +0100

    changes

--- routes/web.php
+++ routes/web.php
@@ -140,22 +140,22 @@ Route::post('/send-contact-message', [UserController::class,'sendContact'])->nam
 Route::post('/make-payment', [PaymentController::class, 'makePayment'])->name('pay');
 });
 
-Auth::routes(['verify' => true]);
+// Auth::routes(['verify' => true]);
 
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
 
-    return redirect('/redirects');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+//     return redirect('/redirects');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
 
 
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
 
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
